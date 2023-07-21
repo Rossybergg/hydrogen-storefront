7166a4ba8c28240ef2eea01b7f8a0190365874dd
Ross Hunter <blue_lizzard@hotmail.com>
2023-07-21 17:24:35 +0100

    created collections

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,5 @@
+import {Link, useLoaderData} from '@remix-run/react';
+import {Image} from '@shopify/hydrogen';
 export const meta = () => {
   return [
     {title: 'Hydrogen'},
@@ -5,16 +7,58 @@ export const meta = () => {
   ];
 };
 
-const Index = () => {
-  return (
-    <>
-      <h3> Successfully connected to the Shopify APIs</h3>
+export const loader = async ({context}) => {
+  return await context.storefront.query(COLLECTIONS_QUERY);
+};
 
-      <h4>
-        Hello from Shopify Jake!!!! Your bro is now a Shopify Developer...
-      </h4>
-    </>
+const Index = () => {
+  const {collections} = useLoaderData();
+  return (
+    <section className="w-full gap-4">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+        Collections
+      </h2>
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+        {collections.nodes.map((collection) => {
+          return (
+            <Link to={`/collections/${collection.handle}`} key={collection.id}>
+              <div className="grid gap-4">
+                {collection?.image && (
+                  <Image
+                    alt={`Image of ${collection.title}`}
+                    data={collection.image}
+                    key={collection.id}
+                    sizes="(max-width: 32em) 100vw, 33vw"
+                  />
+                )}
+                <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
+                  {collection.title}
+                </h2>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
   );
 };
+
+const COLLECTIONS_QUERY = `#graphql
+  query FeaturedCollections {
+    collections(first: 3, query: "collection_type:smart") {
+      nodes {
+        id
+        title
+        handle
+        image {
+          altText
+          width
+          height
+          url
+        }
+      }
+    }
+  }
+`;
 
 export default Index;
