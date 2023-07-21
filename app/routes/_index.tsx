6befa2367b8f37254dc537cea8f79674aef21949
Ross Hunter <blue_lizzard@hotmail.com>
2023-07-21 16:57:29 +0100

    installed tailwind, created layout

---
+++ app/routes/_index.tsx
@@ -0,0 +1,20 @@
+export const meta = () => {
+  return [
+    {title: 'Hydrogen'},
+    {description: 'A custom storefront powered by Hydrogen'},
+  ];
+};
+
+const Index = () => {
+  return (
+    <>
+      <h3> Successfully connected to the Shopify APIs</h3>
+
+      <h4>
+        Hello from Shopify Jake!!!! Your bro is now a Shopify Developer...
+      </h4>
+    </>
+  );
+};
+
+export default Index;
--- remix.config.js
+++ remix.config.js
@@ -4,6 +4,7 @@ module.exports = {
   ignoredRouteFiles: ['**/.*'],
   watchPaths: ['./public'],
   server: './server.ts',
+  tailwind: true,
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
    */
